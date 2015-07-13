dc2d58886a1c3e1e3f10a7c78be88c5074dd5757
Janet Yeilding <janety@microsoft.com>
2015-07-13 14:14:02 -0700

    Add TwitterClient

---
+++ DataGenerators/TwitterClient/TwitterClient/Entities/Sentiment140.cs
@@ -0,0 +1,153 @@
+﻿//********************************************************* 
+// 
+//    Copyright (c) Microsoft. All rights reserved. 
+//    This code is licensed under the Microsoft Public License. 
+//    THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF 
+//    ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY 
+//    IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR 
+//    PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT. 
+// 
+//*********************************************************
+
+using Newtonsoft.Json.Linq;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace TwitterClient
+{
+    public static class Sentiment
+    {
+        public static TwitterPayload ComputeScore(Tweet tweet, string twitterKeywords)
+        {
+
+            return new TwitterPayload
+            {
+                ID = tweet.Id,
+                CreatedAt = ParseTwitterDateTime(tweet.CreatedAt),
+                UserName = tweet.User != null ? tweet.User.Name : null,
+                TimeZone = tweet.User != null ? (tweet.User.TimeZone != null ? tweet.User.TimeZone : "(unknown)") : "(unknown)", 
+                ProfileImageUrl = tweet.User != null ? (tweet.User.ProfileImageUrl != null ? tweet.User.ProfileImageUrl : "(unknown)") : "(unknown)",
+                Text = tweet.Text,
+                Language = tweet.Language != null ? tweet.Language : "(unknown)",
+                RawJson = tweet.RawJson,
+                SentimentScore = (int)Analyze(tweet.Text),
+                Topic = DetermineTopc(tweet.Text, twitterKeywords),
+            };
+        }
+
+        static DateTime ParseTwitterDateTime(string p)
+        {
+            if (p == null)
+                return DateTime.Now;
+            p = p.Replace("+0000 ", "");
+            DateTimeOffset result;
+
+            if (DateTimeOffset.TryParseExact(p, "ddd MMM dd HH:mm:ss yyyy", CultureInfo.GetCultureInfo("en-us").DateTimeFormat, DateTimeStyles.AssumeUniversal, out result))
+                return result.DateTime;
+            else
+                return DateTime.Now;
+        }
+
+        enum SentimentScore
+        {
+            Positive = 4,
+            Neutral = 2,
+            Negative = 0,
+            Undefined = -1
+        }
+
+        static SentimentScore Analyze(string textToAnalyze)
+        {
+            try
+            {
+                string url = string.Format("http://www.sentiment140.com/api/classify?text={0}",
+                                            HttpUtility.UrlEncode(textToAnalyze, Encoding.UTF8));
+                var response = HttpWebRequest.Create(url).GetResponse();
+
+                using (var streamReader = new StreamReader(response.GetResponseStream()))
+                {
+                    try
+                    {
+                        // Read from source
+                        var line = streamReader.ReadLine();
+
+                        // Parse
+                        var jObject = JObject.Parse(line);
+
+                        int polarity = jObject.SelectToken("results", true).SelectToken("polarity", true).Value<int>();
+                        switch (polarity)
+                        {
+                            case 0: return SentimentScore.Negative;
+                            case 4: return SentimentScore.Positive;
+                            // 2 or others
+                            default: return SentimentScore.Neutral;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        return SentimentScore.Neutral;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Sentiment calculation FAILED with:/n{0}", e);
+                return SentimentScore.Neutral;
+            }
+        }
+
+        /// <summary>
+        /// This is a simple text analysis from the twitter text based on some keywords
+        /// </summary>
+        /// <param name="tweetText"></param>
+        /// <param name="keywordFilters"></param>
+        /// <returns></returns>
+        static string DetermineTopc(string tweetText, string keywordFilters)
+        {
+            if (string.IsNullOrEmpty(tweetText))
+                return string.Empty;
+
+            string subject = string.Empty;
+
+            //keyPhrases are specified in app.config separated by commas.  Can have no leading or trailing spaces.  Example of key phrases in app.config
+            //	<add key="twitter_keywords" value="Microsoft, Office, Surface,Windows Phone,Windows 8,Windows Server,SQL Server,SharePoint,Bing,Skype,XBox,System Center"/><!--comma to spit multiple keywords-->
+            string[] keyPhrases = keywordFilters.Split(',');
+
+            foreach (string keyPhrase in keyPhrases)
+            {
+                subject = keyPhrase;
+
+                //a key phrase may have multiple key words, like: Windows Phone.  If this is the case we will only assign it a subject if both words are 
+                //included and in the correct order. For example, a tweet will match if "Windows 8" is found within the tweet but will not match if
+                // the tweet is "There were 8 broken Windows".  This is not case sensitive
+
+                //Creates one array that breaks the tweet into individual words and one array that breaks the key phrase into individual words.  Within 
+                //This for loop another array is created from the tweet that includes the same number of words as the keyphrase.  These are compared.  For example,
+                // KeyPhrase = "Microsoft Office" Tweet= "I Love Microsoft Office"  "Microsoft Office" will be compared to "I Love" then "Love Microsoft" and 
+                //Finally "Microsoft Office" which will be returned as the subject.  if no match is found "Do Not Include" is returned. 
+                string[] KeyChunk = keyPhrase.Trim().Split(' ');
+                string[] tweetTextChunk = tweetText.Split(' ');
+                string Y;
+                for (int i = 0; i <= (tweetTextChunk.Length - KeyChunk.Length); i++)
+                {
+                    Y = null;
+                    for (int j = 0; j <= (KeyChunk.Length - 1); j++)
+                    {
+                        Y += tweetTextChunk[(i + j)] + " ";
+                    }
+                    if (Y != null) Y = Y.Trim();
+                    if (Y.ToUpper().Contains(keyPhrase.ToUpper()))
+                    {
+                        return subject;
+                    }
+                }
+            }
+
+            return "Unknown";
+        }
+    }
+}
---
+++ DataGenerators/TwitterClient/TwitterClient/Entities/TwitterData.cs
@@ -0,0 +1,225 @@
+//********************************************************* 
+// 
+//    Copyright (c) Microsoft. All rights reserved. 
+//    This code is licensed under the Microsoft Public License. 
+//    THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF 
+//    ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY 
+//    IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR 
+//    PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT. 
+// 
+//*********************************************************
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TwitterClient
+{
+    public struct TwitterConfig
+    {
+        public readonly string OAuthToken;
+        public readonly string OAuthTokenSecret;
+        public readonly string OAuthConsumerKey;
+        public readonly string OAuthConsumerSecret;
+        public readonly string Keywords;
+
+        public TwitterConfig(string oauthToken, string oauthTokenSecret, string oauthConsumerKey, string oauthConsumerSecret, string keywords)
+        {
+            OAuthToken = oauthToken;
+            OAuthTokenSecret = oauthTokenSecret;
+            OAuthConsumerKey = oauthConsumerKey;
+            OAuthConsumerSecret = oauthConsumerSecret;
+            Keywords = keywords;
+        }
+    }
+
+    [DataContract]
+    public class TwitterUser
+    {
+        [DataMember(Name = "time_zone")]               public string TimeZone;
+        [DataMember(Name = "name")]                    public string Name;
+        [DataMember(Name = "profile_image_url")]       public string ProfileImageUrl;
+    }
+
+    [DataContract]
+    public class Tweet
+    {
+        [DataMember(Name = "id")]                      public Int64 Id;
+        [DataMember(Name = "in_reply_to_status_id")]   public Int64? ReplyToStatusId;
+        [DataMember(Name = "in_reply_to_user_id")]     public Int64? ReplyToUserId;
+        [DataMember(Name = "in_reply_to_screen_name")] public string ReplyToScreenName;
+        [DataMember(Name = "retweeted")]               public bool Retweeted;
+        [DataMember(Name = "text")]                    public string Text;
+        [DataMember(Name = "lang")]                    public string Language;
+        [DataMember(Name = "source")]                  public string Source;
+        [DataMember(Name = "retweet_count")]           public string RetweetCount;
+        [DataMember(Name = "user")]                    public TwitterUser User;
+        [DataMember(Name = "created_at")]              public string CreatedAt;
+        [IgnoreDataMember]                             public string RawJson;
+
+        public static IEnumerable<Tweet> StreamStatuses(TwitterConfig config)
+        {
+            DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(Tweet));
+
+            var streamReader = ReadTweets(config);
+
+            while (true)
+            {
+                string line = null;
+                try { line = streamReader.ReadLine(); }
+                catch (Exception) { }
+
+                if (!string.IsNullOrWhiteSpace(line) && !line.StartsWith("{\"delete\""))
+                {
+                    var result = (Tweet)jsonSerializer.ReadObject(new MemoryStream(Encoding.UTF8.GetBytes(line)));
+                    result.RawJson = line;
+                    yield return result;
+                }
+
+                // Oops the Twitter has ended... or more likely some error have occurred.
+                // Reconnect to the twitter feed.
+                if (line == null)
+                {
+                    streamReader = ReadTweets(config);
+                }
+            }
+        }
+
+        static TextReader ReadTweets(TwitterConfig config)
+        {
+            var oauth_version = "1.0";
+            var oauth_signature_method = "HMAC-SHA1";
+
+            // unique request details
+            var oauth_nonce = Convert.ToBase64String(new ASCIIEncoding().GetBytes(DateTime.Now.Ticks.ToString()));
+            var oauth_timestamp = Convert.ToInt64(
+                (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc))
+                    .TotalSeconds).ToString();
+
+            var resource_url = "https://stream.twitter.com/1.1/statuses/filter.json";
+
+            // create oauth signature
+            var baseString = string.Format(
+                "oauth_consumer_key={0}&oauth_nonce={1}&oauth_signature_method={2}&" +
+                "oauth_timestamp={3}&oauth_token={4}&oauth_version={5}&track={6}",
+                config.OAuthConsumerKey,
+                oauth_nonce,
+                oauth_signature_method,
+                oauth_timestamp,
+                config.OAuthToken,
+                oauth_version,
+                Uri.EscapeDataString(config.Keywords));
+
+            baseString = string.Concat("POST&", Uri.EscapeDataString(resource_url), "&", Uri.EscapeDataString(baseString));
+
+            var compositeKey = string.Concat(Uri.EscapeDataString(config.OAuthConsumerSecret),
+            "&", Uri.EscapeDataString(config.OAuthTokenSecret));
+
+            string oauth_signature;
+            using (var hasher = new HMACSHA1(ASCIIEncoding.ASCII.GetBytes(compositeKey)))
+            {
+                oauth_signature = Convert.ToBase64String(
+                hasher.ComputeHash(ASCIIEncoding.ASCII.GetBytes(baseString)));
+            }
+
+            // create the request header
+            var authHeader = string.Format(
+                "OAuth oauth_nonce=\"{0}\", oauth_signature_method=\"{1}\", " +
+                "oauth_timestamp=\"{2}\", oauth_consumer_key=\"{3}\", " +
+                "oauth_token=\"{4}\", oauth_signature=\"{5}\", " +
+                "oauth_version=\"{6}\"",
+                Uri.EscapeDataString(oauth_nonce),
+                Uri.EscapeDataString(oauth_signature_method),
+                Uri.EscapeDataString(oauth_timestamp),
+                Uri.EscapeDataString(config.OAuthConsumerKey),
+                Uri.EscapeDataString(config.OAuthToken),
+                Uri.EscapeDataString(oauth_signature),
+                Uri.EscapeDataString(oauth_version)
+            );
+
+
+            // make the request
+            ServicePointManager.Expect100Continue = false;
+
+            var postBody = "track=" + config.Keywords;
+            resource_url += "?" + postBody;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(resource_url);
+            request.Headers.Add("Authorization", authHeader);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.PreAuthenticate = true;
+            request.AllowWriteStreamBuffering = true;
+            request.CachePolicy = new System.Net.Cache.RequestCachePolicy(System.Net.Cache.RequestCacheLevel.BypassCache);
+
+            // bail out and retry after 5 seconds
+            var tresponse = request.GetResponseAsync();
+            if (tresponse.Wait(5000))
+                return new StreamReader(tresponse.Result.GetResponseStream());
+            else
+            {
+                request.Abort(); 
+                return StreamReader.Null;
+            }
+        }
+    }
+
+
+    public class TwitterPayload
+    {
+        public Int64 ID;
+        public DateTime CreatedAt;
+        public string UserName;
+        public string TimeZone;
+        public string ProfileImageUrl;
+        public string Text;
+        public string Language;
+        public string Topic;
+        public int SentimentScore;
+
+        public string RawJson;
+
+        public override string ToString()
+        {
+            return new { ID, CreatedAt, UserName, TimeZone, ProfileImageUrl, Text, Language, Topic, SentimentScore }.ToString();
+        }
+    }
+
+    public class Payload
+    {
+        public DateTime CreatedAt;
+        public string Topic;
+        public int SentimentScore;
+                
+        public override string ToString()
+        {
+            return new {  CreatedAt,  Topic, SentimentScore }.ToString();
+        }
+    }
+
+    public class TwitterMin
+    {
+        public Int64 ID;
+        public DateTime CreatedAt;
+        public string UserName;
+//        public string TimeZone;
+//        public string ProfileImageUrl;
+        public string Text;
+//        public string Language;
+        public string Topic;
+        public int SentimentScore;
+
+//        public string RawJson;
+
+        public override string ToString()
+        {
+            return new { ID, CreatedAt, UserName, Text, Topic, SentimentScore }.ToString();
+        }
+    }
+
+}
