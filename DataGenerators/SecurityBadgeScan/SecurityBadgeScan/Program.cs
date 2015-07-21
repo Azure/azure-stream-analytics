47396bd4d462ca19c04c62d303a70058286312c6
Dipanjan Banik <dipanb@microsoft.com>
2015-07-21 10:33:57 -0700

    Added the Security Badge Scan Sample

---
+++ DataGenerators/SecurityBadgeScan/SecurityBadgeScan/Program.cs
@@ -0,0 +1,44 @@
+﻿//********************************************************* 
+// 
+//    Copyright (c) Microsoft. All rights reserved. 
+//    This code is licensed under the Microsoft Public License. 
+//    THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF 
+//    ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY 
+//    IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR 
+//    PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT. 
+// 
+//*********************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace SecurityBadgeScan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new EventHubConfig();
+                  
+            
+            // Uncomment for picking from Configuration 
+            config.ConnectionString = ConfigurationManager.AppSettings["EventHubConnectionString"];
+            config.EventHubName = ConfigurationManager.AppSettings["EventHubName"];
+                        
+            //To push 1 event per second
+            var eventHubevents = Observable.Interval(TimeSpan.FromSeconds(1)).Select(i => Badge.Generate());
+
+            //To send Data to EventHub as JSON
+            var eventHubDis = eventHubevents.Subscribe(new EventHubObserver(config));
+                                
+            Console.ReadLine();
+            eventHubDis.Dispose();
+                   
+	
+        }
+    }
+}
