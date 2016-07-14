03930f4d842ab81b6447e9daa23960fe2349b58a
Ryan CrawCour <ryancraw@microsoft.com>
2016-07-14 12:56:26 -0700

    first commit of .NET SDK mngt samples

---
+++ Samples/ManageStreamAnalyticsWith.NETSDK/StreamAnalyticsSDKSample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageStreamAnalytics_with_.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
