14828c2c192420a6874d4fbaae9625af48b9f720
Ryan CrawCour <Ryan CrawCour>
2016-07-14 17:47:26 -0700

    Azure auth complete

--- Samples/ManageStreamAnalyticsWith.NETSDK/StreamAnalyticsSDKSample/Program.cs
+++ Samples/ManageStreamAnalyticsWith.NETSDK/StreamAnalyticsSDKSample/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Threading;
+using Microsoft.Azure;
+using Microsoft.Azure.Management.StreamAnalytics;
+using Microsoft.Azure.Management.StreamAnalytics.Models;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
 
 namespace ManageStreamAnalytics_with_.NET
 {
@@ -10,6 +13,56 @@ namespace ManageStreamAnalytics_with_.NET
     {
         static void Main(string[] args)
         {
+            string resourceGroupName = "ASASDK";
+            string streamAnalyticsJobName = "SdkSampleJob";
+            string streamAnalyticsInputName = "Input";
+            string streamAnalyticsOutputName = "Output";
+            string streamAnalyticsTransformationName = "Transformation";
+
+            // Get authentication token
+            TokenCloudCredentials aadTokenCredentials =
+                new TokenCloudCredentials(
+                    ConfigurationManager.AppSettings["SubscriptionId"],
+                    GetAuthorizationHeader());
+
+            // Create Stream Analytics management client
+            StreamAnalyticsManagementClient client = new StreamAnalyticsManagementClient(aadTokenCredentials);
+
+        }
+
+        public static string GetAuthorizationHeader()
+        {
+
+            AuthenticationResult result = null;
+            var thread = new Thread(() =>
+            {
+                try
+                {
+                    var context = new AuthenticationContext(ConfigurationManager.AppSettings["ActiveDirectoryEndpoint"] + ConfigurationManager.AppSettings["ActiveDirectoryTenantId"]);
+
+                    result = context.AcquireToken(
+                        resource: ConfigurationManager.AppSettings["WindowsManagementUri"],
+                        clientId: ConfigurationManager.AppSettings["AsaClientId"],
+                        redirectUri: new Uri(ConfigurationManager.AppSettings["RedirectUri"]),
+                        promptBehavior: PromptBehavior.Always);
+                }
+                catch (Exception threadEx)
+                {
+                    Console.WriteLine(threadEx.Message);
+                }
+            });
+
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Name = "AcquireTokenThread";
+            thread.Start();
+            thread.Join();
+
+            if (result != null)
+            {
+                return result.AccessToken;
+            }
+
+            throw new InvalidOperationException("Failed to acquire token");
         }
     }
 }
