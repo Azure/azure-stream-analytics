8460fa9d063bfb47963c6f0854541166e103537a
Sid Ramadoss <38441940+sidramadoss@users.noreply.github.com>
2019-04-19 13:20:23 -0700

    Update ExampleDeserializer.cs

--- CustomDeserializers/ExampleDeserializer.cs
+++ CustomDeserializers/ExampleDeserializer.cs
@@ -1,4 +1,4 @@
-﻿//********************************************************* 
+//********************************************************* 
 // 
 //    Copyright (c) Microsoft. All rights reserved. 
 //    This code is licensed under the Microsoft Public License. 
@@ -11,20 +11,27 @@
 
 using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
 
 using Microsoft.Azure.StreamAnalytics;
 using Microsoft.Azure.StreamAnalytics.Serialization;
 
 namespace ExampleCustomCode.Serialization
 {
-    /// <summary>
-    /// Example deserializer, creates one CustomEvent for every line.
-    /// </summary>
-    public class ExampleDeserializer : StreamDeserializer<CustomEvent>
+    // Deserializes a stream into objects of type CustomEvent. 
+    // It reads the Stream line by line and assumes each line has three columns separated by ",".
+    // Writes an error to diagnostics and skips the line otherwise.
+    public class CustomCsvDeserializer : StreamDeserializer<CustomEvent>
     {
+        // streamingDiagnostics is used to write error to diagnostic logs
         private StreamingDiagnostics streamingDiagnostics;
-
+        
+        // Initializes the operator and provides context that is required for publishing diagnostics        
+        public override void Initialize(StreamingContext streamingContext)
+        {
+            this.streamingDiagnostics = streamingContext.Diagnostics;
+        }
+        
+        // Deserializes a stream into objects of type CustomEvent        
         public override IEnumerable<CustomEvent> Deserialize(Stream stream)
         {
             using (var sr = new StreamReader(stream))
@@ -32,24 +39,47 @@ namespace ExampleCustomCode.Serialization
                 string line = sr.ReadLine();
                 while (line != null)
                 {
-                    yield return new CustomEvent()
+                    if (line.Length > 0 && !string.IsNullOrWhiteSpace(line))
                     {
-                        Payload = line
-                    };
-
+                        string[] parts = line.Split(',');
+                        if (parts.Length != 3)
+                        {
+                            //if there are not 3 columns in the input, write error to diagnostic log, skip the line and continue deserializing rest of the stream.
+                            streamingDiagnostics.WriteError("Did not get expected number of columns", $"Invalid line: {line}");
+                        }
+                        else
+                        {
+                            // create a new CustomEvent object with 3 values
+                            yield return new CustomEvent()
+                            {
+                                Column1 = parts[0],
+                                Column2 = parts[1],
+                                Column3 = parts[2]
+                            };
+                        }
+                    }
                     line = sr.ReadLine();
                 }
             }
         }
-
-        public override void Initialize(StreamingContext streamingContext)
-        {
-            this.streamingDiagnostics = streamingContext.Diagnostics;
-        }
     }
 
+    /*
+    The CustomEvent class follows the rules mentioned below.
+    
+    All public fields are either:
+        1. One of [long, DateTime, string, double] or their nullableequivalents
+        2. Another struct or class following the same rules
+        3. Array of type <T2> that follows the same rules
+        4. IList`T2` where T2 follows the same rules
+        5. Does not have any recursive types.
+     */
     public class CustomEvent
     {
-        public string Payload { get; set; }
-   }
+        public string Column1 { get; set; }
+
+        public string Column2 { get; set; }
+
+        public string Column3 { get; set; }
+    }
 }
